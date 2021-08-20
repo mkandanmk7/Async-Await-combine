81dd0627d6fc56d310784e5fc8ffd690fa58606b
muthu <mkandanmk7@gmail.com>
2021-08-20 15:11:43 +0530

    all changes done

--- asyncCombine.js
+++ asyncCombine.js
@@ -1,6 +1,6 @@
 // combine Two countires  using Async await:(Asia and Europe)
 
-(async function combine() {
+async function combine() {
   let data = await Promise.all([
     fetch("https://restcountries.eu/rest/v2/region/asia"),
     fetch("https://restcountries.eu/rest/v2/region/europe"),
@@ -8,22 +8,30 @@
 
   let country1 = await data[0].json();
   let country2 = await data[1].json();
-  createFlag([...country1, ...country2]);
-})();
-
-div = document.createElement("div");
-div.className = "outer";
-document.body.append(div);
-
-function createFlag(flags) {
-  flags.forEach((data) => {
-    div.innerHTML += `
-<div class=container>
-<img src=${data.flag}>
-  <h3>${data.name}</h3>
-  <p><b>Region</b>:${data.region}</p>
-  <p><b>Capital</b>:${data.capital}</p>
-  <p><b>Population</b>:${data.population}</p>
-  </div>`;
+  let asiaEurope = [...country1, ...country2];
+  asiaEurope.forEach((details) => {
+    createFlag(details);
   });
+  // createFlag([...country1, ...country2]);
+}
+combine();
+
+function createFlag({ name, flag, population, region, capital }) {
+  const info = document.createElement("div");
+  info.className = "container";
+  info.innerHTML = `
+  <div class="flag-container">
+  <img class="flag" src="${flag} " >
+
+  </div>
+  <div class="details">
+  <h2>${name}</h2>
+  <p><b>Population :</b>${population}</p>
+  <p><b>Region :</b>${region}</p>
+  <p><b>Capital :</b>${capital}</p>
+  </div>
+  
+  
+    `;
+  document.body.append(info);
 }
